55fb9f137e5e1406a29ab140c100f818d3443386
Diwakar <121333748+Diwakar003@users.noreply.github.com>
2024-08-28 21:19:41 +0530

    Add files via upload

---
+++ Skill navigator/FeedbackCollection.java
@@ -0,0 +1,9 @@
+public class FeedbackCollection {
+    public void collectFeedback(Candidate candidate) {
+        System.out.println("Collecting feedback from: " + candidate.getName());
+    }
+
+    public void analyzeFeedback() {
+        System.out.println("Analyzing feedback...");
+    }
+}
