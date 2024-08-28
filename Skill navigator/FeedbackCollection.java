6c6a6d2350cb56edbb33da4c2491e9eaccaac765
Diwakar <121333748+Diwakar003@users.noreply.github.com>
2024-08-28 21:27:57 +0530

    Delete Skill navigator/FeedbackCollection.java

--- Skill navigator/FeedbackCollection.java
+++
@@ -1,9 +0,0 @@
-public class FeedbackCollection {
-    public void collectFeedback(Candidate candidate) {
-        System.out.println("Collecting feedback from: " + candidate.getName());
-    }
-
-    public void analyzeFeedback() {
-        System.out.println("Analyzing feedback...");
-    }
-}
